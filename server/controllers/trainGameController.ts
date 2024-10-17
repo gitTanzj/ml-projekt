27feadb7de4addb685834a4a85cb0e8e211480ca
gitTanzj <kalle.riit@gmail.com>
2024-10-17 08:28:09 +0300

    tensorflow imports yet to be fixed

--- server/controllers/trainGameController.ts
+++ server/controllers/trainGameController.ts
@@ -1,18 +1,43 @@
-import * as tf from '@tensorflow/tfjs-node';
-
+import * as tf from '@tensorflow/tfjs';
+import fs from 'fs';
 import { Request, Response } from 'express';
 
-// const model = tf.sequential();
-// model.add(tf.layers.dense({inputShape: [300], units: 1}))
+// const loadImage = async (path: string) => {
+//     const imageBuffer = fs.readFileSync(path);
+//     const imageTensor = tf.node.decodeImage(imageBuffer, 3)
+//         .resizeNearestNeighbor([224, 224])
+//         .toFloat()
+//         .div(tf.scalar(255.0));
+//     return imageTensor.expandDims(); // Add batch dimension
+// }
+
+// const loadImages = async (paths: string[]) => {
+//     const imageTensors = await Promise.all(paths.map(path => loadImage(path)))
+//     return tf.concat(imageTensors);
+// }
+
+
+
+const model = tf.sequential();
+model.add(tf.layers.dense({inputShape: [300], units: 1}))
 
 const trainModel = async (req: Request, res: Response) => {
-    console.log(
-        req.body
-    )
     const images = req.files as Express.Multer.File[];
     const label = req.body.label;
     console.log(images)
     console.log(label)
+
+    // TRAIN MODEL WITH TRAINING DATA
+
+
+
+    // setTimeout(() => {
+    //     fs.readdirSync('./uploads').forEach(file => {
+    //         fs.rmSync(`./uploads/${file}`)
+    //     })
+    // }, 3000)
+    
+    res.status(201).send('Model trained successfully')
 }
 
 const testModel = async (req: Request, res: Response) => {
